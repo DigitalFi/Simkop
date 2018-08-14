be61f081901dafb8f090c8ceca24f9988e77b1c7
Inge Arfatikka <ingearfatikka@gmail.com>
2018-08-14 14:16:04 +0700

    new commit
    Version terbaru

---
+++ app/src/main/java/com/example/inge/simkopapps/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.inge.simkopapps;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int splashInterval = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(MainActivity.this, Menu_Simkop.class);
+                startActivity(i); // menghubungkan activity splashscren ke main activity dengan intent
+
+
+                //jeda selesai Splashscreen
+                this.finish();
+            }
+
+            private void finish() {
+                // TODO Auto-generated method stub
+
+            }
+        }, splashInterval);
+
+    };
+}
